7f285931b8ecbc22e758373a4aacb6f3b0775a4e
laichao.kai <laichao.kai@alo7.com>
2022-03-18 20:37:40 +0800

    feat: update

--- chain/constants/index.js
+++ chain/constants/index.js
@@ -1,5 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x71132eb56095621c50b92eBf7354DC9f97bE796a";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://my-dapp-lab.vercel.app/api/";
+
 /**/
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL  };
--- webapp/src/constants/index.js
+++ webapp/src/constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
       "type": "receive"
     }
   ]
-export const NFT_CONTRACT_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+export const NFT_CONTRACT_ADDRESS = "0x7239Bad985D788Fe095dA26CC8970b831D06F766"
